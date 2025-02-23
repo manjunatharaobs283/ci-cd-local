c1bda1c7f44389a87b4741fa22ff3e11abf2a9f2
Manjunatha Rao B S <manjunatharaobs@Manjunathas-Mac-mini.local>
2025-02-23 22:28:16 +0530

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello from Local CI/CD! 🚀"));
+app.get("/", (req, res) => res.send("Hello from manju Local CI/CD! 🚀"));
 app.listen(3000, () => console.log("Server running on port 3000"));
 
